e336bdc1431dc6a58bdf1d923a10b9105e3c4141
zhaodafei111 <1097625354@qq.com>
2024-11-30 15:48:18 +0800

    时钟init

--- miniprogram/envList.js
+++
@@ -1,6 +0,0 @@
-const envList = [{"envId":"dafe-4e9c07","alias":"dafei"}]
-const isMac = false
-module.exports = {
-    envList,
-    isMac
-}
---
+++ pages/index/index.js
@@ -0,0 +1,48 @@
+// index.js
+// const app = getApp()
+
+Page({
+  data: {
+    feiTime:"2022年8月8日23:46:11",
+    nowDate: '2022-12-31 23:59:59', //结束时间
+    countdown: '', //倒计时
+    days: '00', //天
+    hours: '00', //时
+    minutes: '00', //分
+    seconds: '00', //秒
+  },
+  countTime() {
+    let days,hours, minutes, seconds;
+    let nowDate = this.data.nowDate;
+    console.log(nowDate)
+    let that = this;
+    let now = new Date().getTime();
+    let end = new Date(new Date().toLocaleDateString()).getTime() + 24 * 60 * 60 * 1000 - 1
+    // console.log("开始时间：" + now, "截止时间:" + end);
+    let leftTime = end - now; //时间差
+    if (leftTime >= 0) {
+      days = Math.floor(leftTime / 1000 / 60 / 60 / 24);
+      hours = Math.floor(leftTime / 1000 / 60 / 60 % 24);
+      minutes = Math.floor(leftTime / 1000 / 60 % 60);
+      seconds = Math.floor(leftTime / 1000 % 60);
+      seconds = seconds < 10 ? "0" + seconds : seconds
+      minutes = minutes < 10 ? "0" + minutes : minutes
+      hours = hours < 10 ? "0" + hours : hours
+      that.setData({
+        countdown: days+":"+hours + "：" + minutes + "：" + seconds,
+        days,
+        hours,
+        minutes,
+        seconds
+      })
+      setTimeout(that.countTime, 1000);
+    } else {
+      that.setData({
+        countdown: '已截止'
+      })
+    }
+  },
+  onLoad: function (options) {
+    this.countTime();
+  },
+});
